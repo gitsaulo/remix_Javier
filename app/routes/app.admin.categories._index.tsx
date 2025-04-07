8c1050d34af5034dd17d00b73e2d51e424ee8c67
RubenMMPS <rmartinmotilla@gmail.com>
2025-04-07 12:12:49 +0200

    clase 07/04/2025

--- app/routes/app.admin.categories._index.tsx
+++ app/routes/app.admin.categories._index.tsx
@@ -1,54 +1,45 @@
+import { useLoaderData } from "@remix-run/react";
+import { DiscoverGrid, DiscoverListItem } from "~/components/discover";
+import db from "~/db.server";
+
+export async function loader() {
+  const categories = await db.category.findMany({
+    take: 25,
+    orderBy: {
+      updatedAt: "desc",
+    }
+  });
+  return { categories };
+}
+
+type DiscoverRecipeHeaderProps = {
+  category: {
+    name: string;
+    thumb: string;
+    description: string;
+  }
+};
+
 export default function AdminCategories() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
+    <div className="h-[calc(100vh-1rem)] p-4 m-[-1rem] overflow-auto">
+      <h1 className="text-2xl font-bold mb-4">CATEGORIES</h1>
+      <DiscoverGrid>
+        {data.categories.map((category) => (
           <div>
-            <h2>Beef</h2>
-          </div>
+          <a href={"/app/admin/categories/"+category.name}>
+            <div>
+              <img alt="Beef" src={`/images/categories/${category.thumb}`} />
+              <div>
+                <h2>{category.name}</h2>
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
-          <div>
-            <h2>Beef</h2>
-          </div>
-        </div>
-      </a>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
-          <div>
-            <h2>Beef</h2>
-          </div>
-        </div>
-      </a>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
-          <div>
-            <h2>Beef</h2>
-          </div>
-        </div>
-      </a>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
-          <div>
-            <h2>Beef</h2>
-          </div>
-        </div>
-      </a>
-      <a href="/app/admin/categories/Beef">
-        <div>
-          <img alt="Beef" src="/images/categories/beef.png" />
-          <div>
-            <h2>Beef</h2>
-          </div>
-        </div>
-      </a>
+        ))}
+      </DiscoverGrid>
     </div>
-  );
+  );  
 }
